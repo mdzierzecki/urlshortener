ce7e856bf2f5fff4adeeed81cecebf368cf890d2
Mateusz Dzierzecki <mateusz@dzierzecki.com>
2020-03-31 12:54:26 +0200

    Set up connection to Posgresql db

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shortenerdb',
+        'USER': 'shortenerusr',
+        'PASSWORD': 'shortenerpass222',
+        'HOST': 'localhost',
+        'PORT': '',
+    },
 }
 
 
