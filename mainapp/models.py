b29b67017867b233e07655be82f9535fa7509f1c
Mateusz Dzierzecki <mateusz@dzierzecki.com>
2020-03-31 13:04:17 +0200

    Url model created

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+# Url model
+class Url(models.Model):
+    target_url = models.TextField(null=False, blank=False)
+    shortened_url = models.CharField(max_length=10, null=True, blank=True)
+
+    created_at = models.DateTimeField(
+        _("Created at"),
+        auto_now_add=True,
+        help_text=_("When url was shortened")
+    )
+
+    def __str__(self):
+        return self.target_url
